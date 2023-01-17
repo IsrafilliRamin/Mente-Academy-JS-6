75806ea505678229bfd7bb877e79c83f2a9eb7af
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2023-01-17 15:22:23 +0400

    Add files via upload

---
+++ js-6/app.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+/* Array push metodu
+    
+masivin sonuna element elave etmek ucun
+
+*/
+/* Array unshift metodu
+    
+masivin evveline element elave etmek ucun
+
+*/
+/* Array pop metodu
+    
+masivin sonuna elementi silir 
+
+*/
+/* Array shift metodu
+    
+masivin evvelindeki elementi silir
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* DOM -  */
+/* Queryselectorla */
+/* const p = document.querySelector(".p");
+const divId = document.querySelector("#divId");
+const pA = document.querySelectorAll(".p") */
+/* console.log(divId); */
+/* ancaq Idleri ById */
+/* const constpId = document.getElementById("Hello")
+const clasDIv = document.getElementsByClassName("getClass")[0] */
+
+/* console.log(clasDIv); */
+/* console.log(constpId); */
+
+
+/* const byName = document.getElementsByTagName("div")[0];
+
+console.log(byName);
+ */
+let div = document.createElement("div");
+div.innerText = "Developerler"
+div.className = "div1"
+div.id = "divId"
+console.log(div);
+ document.body.appendChild(div)
+
+
+
+
+
+
+
